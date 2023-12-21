cba44c6c51d03eba8f62ef1e15aa3c55f7960fb7
usernameabc <abc@gmail.com>
2023-12-21 10:23:51 +0800

    fix error

--- Car Monitoring System/Form1.vb
+++ Car Monitoring System/Form1.vb
@@ -11,7 +11,22 @@ Partial Public Class Form1
         InitializeComponent()
     End Sub
 
+<<<<<<< HEAD
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+
+=======
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MsgBox("test sample 1")
+
+        msgbox("Test sample 2 from rizal")
+>>>>>>> c45ec5521c6bf9bf4dcfa0e407a776b01f790480
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 End Class
