f6c2f88db756048db7c173d68669606749881c4f
nko <nkosmsn@gmail.com>
2023-12-21 10:37:19 +0800

    add another text box

--- Car Monitoring System/Form1.vb
+++ Car Monitoring System/Form1.vb
@@ -11,27 +11,25 @@ Partial Public Class Form1
         InitializeComponent()
     End Sub
 
-<<<<<<< HEAD
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         TextBox1.Text = ""
         TextBox2.Text = ""
     End Sub
 
 
-=======
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MsgBox("test sample 1")
 
         msgbox("Test sample 2 from rizal")
->>>>>>> c45ec5521c6bf9bf4dcfa0e407a776b01f790480
-    End Sub
+         End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
 
     Private Sub TextEdit1_EditValueChanged(sender As Object, e As EventArgs) Handles TextEdit1.EditValueChanged
         TextEdit1.EditValue = "text edit 4"
+        TextEdit1.EditValue = ""
 
     End Sub
 End Class
