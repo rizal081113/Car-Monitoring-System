86391a4d0e8afea315cf0055081c51f673d26ff5
usernameabc <abc@gmail.com>
2023-12-21 10:04:48 +0800

    add text box (3) and 1 button

--- Car Monitoring System/Form1.vb
+++ Car Monitoring System/Form1.vb
@@ -10,4 +10,10 @@ Partial Public Class Form1
     Public Sub New()
         InitializeComponent()
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+
+    End Sub
 End Class
